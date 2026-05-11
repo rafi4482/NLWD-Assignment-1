feb2e09bfcd1ac26ad2ee74d59c717ee2978d31c
rafi4482 <rafi.kazla@gmail.com>
2026-05-11 10:40:38 +0600

    Problem 6 and 7 done

--- solutions.ts
+++ solutions.ts
@@ -34,3 +34,64 @@ function checkType(strnum: string | number) {
 }
 
 //Problem 4
+
+//Problem 5
+interface Book {
+    title: string
+    author: string
+    publishedYear: number
+}
+
+function toggleReadStatus(book: Book) {
+    return {
+        ...book,
+        isRead: true
+    }
+}
+
+const myBook: Book = {
+    title: "TypeScript Guide",
+    author: "Jane Doe",
+    publishedYear: 2024
+}
+
+//Problem 6
+class Person {
+  name: string
+  age: number
+
+  constructor(name: string, age: number) {
+    this.name = name
+    this.age = age
+  }
+}
+
+class Student extends Person {
+  grade: string
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age)
+    this.grade = grade
+  }
+
+  getDetails(): string {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`
+  }
+}
+
+//Problem 7
+function getIntersection(arr1: number[], arr2: number[]): number[] {
+  let result: number[] = []
+
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        result[result.length] = arr1[i]
+      }
+    }
+  }
+
+  return result
+}
+
+console.log(getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7]))
